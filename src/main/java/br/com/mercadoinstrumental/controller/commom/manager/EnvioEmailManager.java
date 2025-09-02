d8213218a8e19a8d71a9a74badb46dc02003e0b5
Wellison Silva <wellis_on@hotmail.com>
2025-09-01 23:38:25 -0300

    Remover Mailgun API key do código

--- src/main/java/br/com/mercadoinstrumental/controller/commom/manager/EnvioEmailManager.java
+++ src/main/java/br/com/mercadoinstrumental/controller/commom/manager/EnvioEmailManager.java
@@ -21,25 +21,28 @@ public class EnvioEmailManager {
 
 	
 	public void enviarEmailHtml(List<String> destinatarios, String titulo, String corpoHtml) {
-	
+
 	    if (destinatarios == null || destinatarios.isEmpty()) {
 	        throw new IllegalArgumentException("A lista de destinatários não pode ser nula ou vazia.");
 	    }
-	
+
 	    for (String destinatario : destinatarios) {
 	        try {
 	            MimeMessage message = mailSender.createMimeMessage();
 	            MimeMessageHelper helper = new MimeMessageHelper(message, "UTF-8");
-	            
+
+	            helper.setFrom("suporte@mercadoinstrumental.com.br", "Mercado Instrumental");
+
 	            helper.setTo(destinatario);
 	            helper.setSubject(titulo);
 	            helper.setText(corpoHtml, true); // o segundo parâmetro 'true' indica que o texto é HTML
-	
+
 	            mailSender.send(message);
 	        } catch (Exception e) {
 	            System.err.println("Erro ao enviar e-mail HTML para " + destinatario + ": " + e.getMessage());
 	        }
 	    }
 	}
+
 	
 }
